6673c7d6807fbac397cdf870f68d8bfb3b60465c
Tobias Knapp <tobias.knapp@iti-software.de>
2018-04-06 11:49:36 +0200

    initial checkin

--- MendauConsole/Program.cs
+++ MendauConsole/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Cognitive.CustomVision.Training;
 
 namespace MendauConsole
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var trainingKey = ConfigurationManager.AppSettings["TrainingKey"];
+            var projectName = ConfigurationManager.AppSettings["ProjectName"]; 
+            var imageFolder = ConfigurationManager.AppSettings["ImageFolder"];
+     
+            var trainingApi = new TrainingApi() {ApiKey = trainingKey };
+
+            var projects = trainingApi.GetProjects();
+            var project = projects.SingleOrDefault(x => x.Name == projectName) ?? trainingApi.CreateProject(projectName);
+
+            var files = Directory.GetFiles(imageFolder);
+
+            var tags = trainingApi.GetTags(project.Id);
+
+            foreach (var file in files)
+            {
+                var tagName = Path.GetFileNameWithoutExtension(file);
+                tagName = tagName.Substring(tagName.LastIndexOf("IMG", StringComparison.Ordinal) + 4, 4);
+
+                try
+                {
+                    var tag = tags.Tags.FirstOrDefault(x => x.Name == tagName);
+                    if (tag == null)
+                    {
+                        tag = trainingApi.CreateTag(project.Id, tagName);
+                        tags.Tags.Add(tag);
+                    }
+
+                    using (var stream = new MemoryStream(File.ReadAllBytes(file)))
+                    {
+                        trainingApi.CreateImagesFromData(project.Id, stream, new List<string>() { tag.Id.ToString() });
+                    }
+                }
+                catch (Microsoft.Rest.HttpOperationException ex)
+                {
+                    Console.WriteLine(ex.Response.Content);
+                }
+            }
+        
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
         }
     }
 }
